fb5d3d6f23b1c0c3102b278d9766401bb3f5d572
Waleed201 <aannsq23@gmail.com>
2024-10-02 22:42:04 +0300

    fixe header and wrapper margin

--- src/App.jsx
+++ src/App.jsx
@@ -7,46 +7,11 @@ import Header from "./pages/Header";
 import "./css/App.css"
 
 function App() {
-  const [headerHeight, setHeaderHeight] = useState(0);
-  const location = useLocation(); // Tracks the current location (route)
-
-  const adjustBodyMargin = () => {
-    const header = document.getElementById("header");
-    if (header) {
-      setHeaderHeight(header.offsetHeight);
-    }
-  };
-
-  useEffect(() => {
-    // Adjust margin on initial load
-    const timer = setTimeout(() => {
-      adjustBodyMargin();
-    }, 100); // Adjust the delay as necessary
-  
-    return () => clearTimeout(timer);
-  
-    // Adjust margin when window is resized or orientation changes
-    window.addEventListener("resize", adjustBodyMargin);
-    window.addEventListener("orientationchange", adjustBodyMargin);
-    window.addEventListener("load", adjustBodyMargin);
-
-    // Cleanup the event listeners when component unmounts
-    return () => {
-      window.removeEventListener("resize", adjustBodyMargin);
-      window.removeEventListener("orientationchange", adjustBodyMargin);
-      window.removeEventListener("load", adjustBodyMargin);
-    };
-  }, []);
-
-  useEffect(() => {
-    // Adjust margin whenever the route changes
-    adjustBodyMargin();
-  }, [location]); // Triggers when route changes
 
   return (
     <div>
       <Header /> {/* Add id to Header component */}
-      <div className="Wrapper" style={{ marginTop: `${headerHeight}px` }}> 
+      <div className="Wrapper"> 
         <Routes>
           <Route path="/" element={<Home />} /> 
           <Route path="/contact" element={<Contact />} /> 
